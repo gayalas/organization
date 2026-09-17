744011fa139b33c72a4f18af57dd4af82475d639
gayalas <gumoaysan@gmail.com>
2026-09-17 18:32:11 -0400

    unnecesary things

--- OrganizationApi/OrganizationApi.Infraestructure/obj/Debug/net10.0/OrganizationApi.Infraestructure.AssemblyInfo.cs
+++ OrganizationApi/OrganizationApi.Infraestructure/obj/Debug/net10.0/OrganizationApi.Infraestructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrganizationApi.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+171a7d46c3f725f06c4df92084654704a5921238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1072c56cf89a59bf1c08798f466472ca3d7d0e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrganizationApi.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrganizationApi.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
